d4d739b0170d5c2848c35e66cb7eb8a1c1b9c7b3
Sergey Sidorov <89048598831s@gmail.com>
2022-12-08 01:43:36 +0300

    done

---
+++ tasks/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пробелы меняем на черточки
+// Маленькие "к" меняем на большие "К"
+// Большие "С" меняем на маленькие "с"
+
+// Ясна ли задача?
+
+string text ="Вы также можете расскомментировать код с помощью той же команды."
+            +" Выберите код, который в данный момент закомментирован, и выполните"
+            +" команду быстрого доступа. Он снова станет незакомментированным";
+//string s= "qwerty"
+//           012345
+//s[3] -обращение к 3му(по факту к 4му) символу строки 
+
+string Replace(string text, char oldValue, char newValue)
+{
+   string result = String.Empty;
+   int length = text.Length;
+   for (int i=0; i<length;i++)
+   {
+    if (text[i] == oldValue) result= result +$"{newValue}";
+    else result= result+$"{text[i]}";
+   }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText=Replace(text, 'к', 'К');
+Console.WriteLine(newText);
---
+++ цикл for/Program.cs
@@ -0,0 +1,30 @@
+﻿// string Method4(int count, string text)
+// {
+//     int i=0;
+//     string result=String.Empty; // переменная result будет пустой с помощью этой команды
+
+//     while(i<count)
+//     {
+//         result= result+text
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)
+{
+    string result=String.Empty; 
+    for (int i=0;i<count;i++) {result= result+text;}
+    return result;
+}
+string res = Method4(10, "s");
+//Console.WriteLine(res);
+
+for(int i=2;i<=10;i++)
+{
+    for (int j=2;j<=10;j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine(); //Чтобы тело цикла на выводе прерывалось пустой строкой
+}
